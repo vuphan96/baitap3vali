8600dda43b7d55467ebaed402e39e92a9c018253
ADMIN <you@example.com>
2022-05-12 01:30:46 +0700

    add new file active.php

---
+++ active.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test Active</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="jquery-3.6.0.min.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+		<ul class="nav">
+			<li class="menu active">1</li>
+			<li class="menu">2</li>
+			<li class="menu">3</li>
+			<li class="menu">4</li>
+			<li class="menu">5</li>
+		</ul>
+	</div>
+<script type="text/javascript" src="script.js">
+	
+</script>
+</body>
+</html>
